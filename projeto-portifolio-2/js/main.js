bc9c22fcf60a5ed63b79c0bfa0e71151a8120959
Betoside <betoside@gmail.com>
2023-08-11 17:41:51 -0300

    Services e Portfolio

--- projeto-portifolio-2/js/main.js
+++ projeto-portifolio-2/js/main.js
@@ -4,7 +4,52 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
+//Services section - Modal
+const serviceModals = document.querySelectorAll('.service-modal');
+const learnmoreBtns = document.querySelectorAll('.learn-more-btn');
+const modalcloseBtns = document.querySelectorAll('.modal-close-btn');
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add('active');
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalcloseBtns.forEach((modalcloseBtn) => {
+    modalcloseBtn.addEventListener('click', () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove('active');
+        });
+    });
+});
+
+
 //Portfolio section - Modal
+const portfolioModals = document.querySelectorAll('.portfolio-model');
+const imgCards = document.querySelectorAll('.img-card');
+const portfolioCloseBtns = document.querySelectorAll('.portfolio-close-btn');
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add('active');
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener('click', () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener('click', () => {
+        portfolioModals.forEach((portifolioModalView) => {
+            portifolioModalView.classList.remove('active');
+        });
+    });
+});
 
 //Our clients - Swiper
 
